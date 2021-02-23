b88471cc280294436bcd3ba10a3bd906c4d2ddb2
Francisco Ramirez <framirez@txmglobal.com>
2021-02-23 12:43:55 -0600

    Apellido3
    Apellido3

--- inicio/index.php
+++ inicio/index.php
@@ -3,5 +3,6 @@ echo "INDEX";
 echo "<label>Nombre:</label>";
 echo "<input type='text' name='nombre' id='nombre'>";
 echo "<label>Apellido:</label>";
-echo "<input type='text' id='apellido2' name='apellido2'>  ";
+echo "<input type='text' id='apellido3' name='apellido3'>  ";
+echo "<label></label>";
 ?>
