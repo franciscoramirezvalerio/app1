61605036c74b527210531357d73f0f4db52dfa93
=[franciscoramirezvalerio] <=[franciscoramirezvalerio@gmail.com]>
2021-02-23 13:55:07 -0600

    hOME
    hOME

--- inicio/index.php
+++ inicio/index.php
@@ -3,6 +3,6 @@ echo "INDEX";
 echo "<label>Nombre:</label>";
 echo "<input type='text' name='nombre' id='nombre'>";
 echo "<label>Apellido:</label>";
-echo "<input type='text' id='apellido6' name='apellido6'>  ";
-echo "<label>Fin lib</label>";
+echo "<input type='text' id='apellido8' name='apellido8'>  ";
+echo "<label>Fin lib.</label>";
 ?>
