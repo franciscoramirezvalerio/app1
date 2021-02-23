4ff38456a2c25ccac4d40c982c3d66689ca0703d
=[franciscormz] <=[franciscoramirezvalerio@hotmail.com]>
2021-02-23 12:41:39 -0600

    apellido2
    apellido2

--- inicio/index.php
+++ inicio/index.php
@@ -3,5 +3,5 @@ echo "INDEX";
 echo "<label>Nombre:</label>";
 echo "<input type='text' name='nombre' id='nombre'>";
 echo "<label>Apellido:</label>";
-echo "<input type='text' id='apellido' name='apellido'>  ";
+echo "<input type='text' id='apellido2' name='apellido2'>  ";
 ?>
