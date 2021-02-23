e6e6185ac4ae832c05bce2fa9c025f2d88b525e2
=[franciscormz] <=[franciscoramirezvalerio@hotmail.com]>
2021-02-23 13:43:58 -0600

    55
    55

--- inicio/index.php
+++ inicio/index.php
@@ -3,6 +3,6 @@ echo "INDEX";
 echo "<label>Nombre:</label>";
 echo "<input type='text' name='nombre' id='nombre'>";
 echo "<label>Apellido:</label>";
-echo "<input type='text' id='apellido4' name='apellido4'>  ";
+echo "<input type='text' id='apellido5' name='apellido5'>  ";
 echo "<label>Fin</label>";
 ?>
