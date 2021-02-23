07678aca01304fd95dbf1c4aa2ad4d7531afa79b
=[franciscoramirezvalerio] <=[franciscoramirezvalerio@gmail.com]>
2021-02-23 14:18:25 -0600

    999
    999

--- inicio/index.php
+++ inicio/index.php
@@ -3,11 +3,6 @@ echo "INDEX";
 echo "<label>Nombre:</label>";
 echo "<input type='text' name='nombre' id='nombre'>";
 echo "<label>Apellido:</label>";
-<<<<<<< Updated upstream
 echo "<input type='text' id='apellido8' name='apellido8'>  ";
 echo "<label>Fin lib.7777</label>";
-=======
-echo "<input type='text' id='apellido7' name='apellido7'>  ";
-echo "<label>Fin</label>";
->>>>>>> Stashed changes
 ?>
